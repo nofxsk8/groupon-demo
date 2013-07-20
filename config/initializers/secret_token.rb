8e54406036c2ae63bbeb1b54cb767f61918c11c4
Alex D <nofx_sk8@abv.bg>
2013-07-20 20:26:59 +0300

    groupon demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'e05411b30f66bded188c580aa76fa98313d9fbc098c594e7ef2d95451c6e85379601fcfff0c2aff08fa1d668029644ce0d3680bbc11f73c90c00348cf732c06f'
